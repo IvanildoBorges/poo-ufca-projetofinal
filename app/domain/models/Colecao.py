ae5f37946e67971aeefc1d58f49c6d777a9261da
Yan Brasil Angelim de Brito <40834307+BrSubs@users.noreply.github.com>
2026-01-05 18:55:18 -0300

    Update Colecao.py
    Criado a classe Colecao e os métodos 
    - adicionar_publicacao
    - listar_publicacoes

--- app/domain/models/Colecao.py
+++ app/domain/models/Colecao.py
@@ -1,3 +1,21 @@
+from app.domain.models.Publicacao import Publicacao
+
 class Colecao:
     def __init__(self):
-        pass
+        self.publicacoes = []
+
+    def adicionar_publicacao(self, publicacao: Publicacao):
+      # Verifica se a publicacão já existe na colecão e adiciona uma nova publicacão à colecão caso ela não exista
+      if publicacao in self.publicacoes:
+          raise ValueError("Publicação já existe na coleção")
+      self.publicacoes.append(publicacao)
+
+    def listar_publicacoes(self):
+      return self.publicacoes
+
+    def __len__(self):
+        return len(self.publicacoes)
+
+    def __iter__(self):
+        # Tornar a classe Colecao iterável
+        return iter(self.publicacoes)
